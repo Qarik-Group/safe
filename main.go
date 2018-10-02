57636f241d4e573268db12f8d42206caa3ab14f9
Thomas Mitchell <klaue@live.com>
2018-10-02 13:25:36 -0400

    Specify default sig-algorithm in help

--- main.go
+++ main.go
@@ -2406,7 +2406,8 @@ The following options are recognized:
                       with. Valid values are md5-rsa, sha1-rsa, sha256-rsa
                       sha384-rsa, sha512-rsa, sha256-rsapss, sha384-rsapss,
                       sha512-rsapss, dsa-sha1, dsa-sha256, ecdsa-sha1,
-                      ecdsa-sha256, ecdsa-sha384, and ecdsa-sha512.
+											ecdsa-sha256, ecdsa-sha384, and ecdsa-sha512. Defaults
+                      to sha512-rsa.
 `,
 	}, func(command string, args ...string) error {
 		rc.Apply(opt.UseTarget)
@@ -2513,7 +2514,8 @@ The following options are recognized:
                       with. Valid values are md5-rsa, sha1-rsa, sha256-rsa
                       sha384-rsa, sha512-rsa, sha256-rsapss, sha384-rsapss,
                       sha512-rsapss, dsa-sha1, dsa-sha256, ecdsa-sha1,
-                      ecdsa-sha256, ecdsa-sha384, and ecdsa-sha512.
+											ecdsa-sha256, ecdsa-sha384, and ecdsa-sha512. Defaults
+                      to sha512-rsa.
 `,
 	}, func(command string, args ...string) error {
 		rc.Apply(opt.UseTarget)
@@ -2624,7 +2626,8 @@ The following options are recognized:
                       with. Valid values are md5-rsa, sha1-rsa, sha256-rsa
                       sha384-rsa, sha512-rsa, sha256-rsapss, sha384-rsapss,
                       sha512-rsapss, dsa-sha1, dsa-sha256, ecdsa-sha1,
-                      ecdsa-sha256, ecdsa-sha384, and ecdsa-sha512.
+											ecdsa-sha256, ecdsa-sha384, and ecdsa-sha512. Defaults
+                      to sha512-rsa.
 `,
 	}, func(command string, args ...string) error {
 		rc.Apply(opt.UseTarget)
